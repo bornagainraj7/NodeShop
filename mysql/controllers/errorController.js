f559e3605c5a3c04009f2b9f826aa906bbaf2438
Rajkumar Pal <bornagain.raj7@gmail.com>
2019-06-21 11:21:51 +0530

    initial file setup

--- mysql/controllers/errorController.js
+++
@@ -1,8 +0,0 @@
-let get404 = (req, res, next) => {
-    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: '404 Page Not Found', path: '/404' });
-}
-
-module.exports = {
-    get404: get404
-}
--- mysql/utils/path.js
+++
@@ -1,5 +0,0 @@
-const path = require('path');
-
-const location = path.dirname(process.mainModule.filename);
-
-module.exports = location;
